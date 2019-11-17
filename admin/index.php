beca8d7fa86cf5623c179e6deab54ef9dc0cb7fb
ledutu <javajava159@gmail.com>
2019-11-17 22:52:12 +0700

    index admin

---
+++ admin/index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Manage</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="index.css?version=51">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!-- application -->
+    <div class="application">
+    
+
+        <div class="header">
+            
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">PHÒNG TRỌ <b style="color: red">LÊ ĐỨC TÙNG</b></a>
+                    </div>
+
+                    <ul class="nav navbar-nav">
+                        
+                        <form class="navbar-form navbar-left" role="search">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                            </div>
+
+                            <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                        </form>
+                        
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right" style="margin-right: 50px">
+                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+                    </ul>
+
+                </div>
+            </nav>
+                    
+        </div> <!-- End header -->
+
+        <!-- Information -->
+        <div class="information">
+            <div class="container-fluid">
+                <div class="row">
+                    
+                    <!-- Left Menu -->
+                    <div class="col-xs-2 col-sm-2 col-md-2">
+                        <h3><i class="fa fa-home fa-lg"></i> Home</h3> 
+
+                        <div class="leftMenu">
+                            <a href="#" style="display: block" ><i class="fa fa-check fa-lg"></i>Phòng trọ đang thuê</a> 
+                            <a href="#" style="display: block" ><i class="fa fa-times fa-lg"></i> Phòng trọ chưa thuê</a> 
+                            <a href="#" style="display: block" ><i class="fa fa-users fa-lg"></i>Thông tin thành viên</a> 
+                            <hr>
+                            <a href="#" style="display: block" data-toggle="modal" data-target="#roomAdding"><i class="fa fa-plus fa-lg"></i>Thêm phòng</a> 
+                        
+                        </div>  <!-- End Left menu -->  
+                    </div>
+
+                    <!-- Content -->
+                    <div class="col-xs-10 col-sm-10 col-md-10">
+                        <h3>Content</h3>
+
+                        <div class="content">
+
+                            <div class="container">
+                                <div class="row">
+                                    
+                                <?php
+                                    for($i = 1; $i<31; $i++)
+                                    {
+                                ?>
+                                    <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2 card">
+                                        <a href="detail.php" style="text-decoration: none">
+                                            <div class="card text-center" style="padding: 40px">
+                                                <img src="https://image.flaticon.com/icons/png/512/18/18625.png" alt="" width="50px">
+                                                <h4 class="text" style="color: black">A<?= $i ?></h4>
+                                                <p class="text-center">0303233423</p>
+                                            </div>
+                                        </a>
+                                    </div>
+
+                                <?php
+                                    }
+                                ?>
+                                    
+                                </div>  <!-- Row -->
+                            </div>  <!-- End container -->
+
+                        </div>  <!-- End Content -->
+                    </div>
+                                                
+
+                </div>  <!-- End Row -->
+            </div>  <!-- End container -->          
+        </div>  <!-- End Information -->
+
+        <?php 
+            require_once("roomAdd.php")
+        ?>
+
+        
+    </div> <!-- End application -->
+    
+
+</body>
+</html>
---
+++ admin/roomAdd.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    
+    <!-- Modal -->
+    <div id="roomAdding" class="modal fade" role="dialog">
+        <div class="modal-dialog" role="document">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    
+                    <h4 class="modal-title">Thêm phòng</h4>
+                </div>
+
+                <div class="modal-body">
+                    
+                    
+                    <form action="" method="POST" role="form">
+                    
+                        <div class="form-group">
+                            <label for="">Tên phòng</label>
+                            <input type="roomName" class="form-control" id="roomName" placeholder="Nhập tên phòng">
+
+                            <label for="">Giá tiền</label>
+                            <select name="cost" id="inputCost" class="form-control">
+                                <option value="">3.000.000 VNĐ</option>
+                                <option value="">3.500.000 VNĐ</option>
+                                <option value="">4.000.000 VNĐ</option>
+                                <option value="">4.500.000 VNĐ</option>
+                            </select>
+                            
+                            <label for="">Diện tích</label>
+                            <select name="area" id="inputArea" class="form-control">
+                                <option value="">20m2</option>
+                                <option value="">25m2</option>
+                                <option value="">30m2</option>
+                            </select>
+                            
+                            
+
+                            <label for="">Gát</label>
+                            <select name="gat" id="inputGat" class="form-control">
+                                <option value="">Có gát</option>
+                                <option value="">Không có gát</option>
+                            </select>
+                            
+
+                        </div>
+                    
+                        <button type="submit" class="btn btn-primary">Thêm</button>
+                    </form>
+                    
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+</body>
+</html>
